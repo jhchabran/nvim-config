c9a63c42bd3d8bef137dc41ea0b229e73f2debe0
Jean-Hadrien Chabran <jh@chabran.fr>
2021-11-27 17:13:07 +0100

    Fix lua language server

--- lua/jh/lua.lua
+++ lua/jh/lua.lua
@@ -1,13 +1,46 @@
 local lspconfig = require('lspconfig')
 
-local luadev = require("lua-dev").setup({
-  lspconfig = {
-    cmd = {'sumneko_lua'},
-    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-  },
-})
+local sumneko_binary_path = vim.fn.exepath('lua-language-server')
+local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+require'lspconfig'.sumneko_lua.setup {
+    cmd = {sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua"};
+    settings = {
+        Lua = {
+        runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+            -- Setup your lua path
+            path = runtime_path,
+        },
+        diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {'vim'},
+        },
+        workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+            enable = false,
+        },
+        },
+    },
+}
+
+-- local luadev = require("lua-dev").setup({
+--   lspconfig = {
+--     cmd = {'sumneko_lua'},
+--     capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+--   },
+-- })
 -- TODO my lua config is broken at the moment
-lspconfig.sumneko_lua.setup(luadev)
+-- lspconfig.sumneko_lua.setup(luadev)
 
 vim.cmd(([[
 autocmd FileType lua lua require'cmp'.setup.buffer {
