0ee25d9fc9844c2640ab7398e7332e26b2e7dc50
Jean-Hadrien Chabran <jh@chabran.fr>
2021-12-14 19:56:50 +0100

    Update firenvim config

--- lua/jh/firenvim.lua
+++ lua/jh/firenvim.lua
@@ -1,7 +1,35 @@
 vim.cmd(([[
 if exists('g:started_by_firenvim')
+  let g:firenvim_config = { 
+    \ 'localSettings': {
+        \ '.*': {
+            \ 'content': 'text',
+            \ 'selector': 'textarea',
+            \ 'takeover': 'never',
+        \ },
+    \ }
+  \ }
+
   set laststatus=0
+
   au BufEnter github.com_*.txt set filetype=markdown
-  set guifont=GoMono\ Nerd\ Font:h16
+  set guifont=JetBrainsMono\ Nerd\ Font\ Mono:h20
+
+  let g:dont_write = v:false
+  function! My_Write(timer) abort
+    let g:dont_write = v:false
+    write
+  endfunction
+
+  function! Delay_My_Write() abort
+    if g:dont_write
+      return
+    end
+    let g:dont_write = v:true
+    call timer_start(10000, 'My_Write')
+  endfunction
+
+  au TextChanged * ++nested call Delay_My_Write()
+  au TextChangedI * ++nested call Delay_My_Write()
 endif
 ]]))
