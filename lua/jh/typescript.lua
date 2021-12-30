d7645de8d8e927988866932e10b65fae5b18cb66
Jean-Hadrien Chabran <jh@chabran.fr>
2021-12-30 17:26:55 +0100

    Update Typescript lsp stuff

--- lua/jh/typescript.lua
+++ lua/jh/typescript.lua
@@ -1,8 +1,27 @@
 local lspconfig = require("lspconfig")
 
 lspconfig.tsserver.setup {
-    on_attach = function(client)
-        client.resolved_capabilities.document_formatting = false
-        on_attach(client)
-    end
+  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+  -- on_attach = function(client)
+    -- client.resolved_capabilities.document_formatting = false
+  on_attach = function(client, bufnr)
+    require("lsp_signature").on_attach({
+      hint_prefix = " ",
+      zindex = 50,
+      bind = true,
+      handler_opts = {
+        border = "none"
+      }
+    })
+  end
 }
+
+vim.cmd(([[
+autocmd FileType typescript lua require'cmp'.setup.buffer {
+\   sources = {
+\     { name = 'vsnip' },
+\     { name = 'nvim_lsp' },
+\   },
+\ }
+autocmd BufWritePre *.ts lua vim.lsp.buf.formatting()
+]]))
