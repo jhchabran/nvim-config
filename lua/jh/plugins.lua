b15d632d43f01a16c8c7c576ebd72dd330cc607d
Jean-Hadrien Chabran <jh@chabran.fr>
2022-01-09 18:56:42 +0100

    Bootstrap packer on fresh clones

--- lua/jh/plugins.lua
+++ lua/jh/plugins.lua
@@ -1,3 +1,12 @@
+-- Boostrap packer
+local fn = vim.fn
+local packer_bootstrap
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 return require("packer").startup(function(use)
   -- Packer can manage itself
   use "wbthomason/packer.nvim"
@@ -405,4 +414,8 @@ return require("packer").startup(function(use)
     require('telescope').load_extension("monarized")
   end}
   use 'marko-cerovac/material.nvim'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
